4017d951e42f43de5f4dc49922c165c64ec82ba2
Evan Shabsove <evan.shabsove@gmail.com>
2016-12-25 14:53:42 -0500

    tried doing stuff but didn't fix anything

--- finalcialApp/app/scripts/services/watchlist-service.js
+++ finalcialApp/app/scripts/services/watchlist-service.js
@@ -78,35 +78,36 @@ angular.module('stockDogApp')
       }
     };
 
-    // Augment watchlists with more helper funtions
+    // Augment Watchlists with additional helper functions
     var WatchlistModel = {
       addStock: function (stock) {
-        var existingStock = _.find(this.stocks, function (s){
+        var existingStock = _.find(this.stocks, function (s) {
           return s.company.symbol === stock.company.symbol;
         });
         if (existingStock) {
-          existingStock.shares += stock.shares
+          existingStock.shares += stock.shares;
         } else {
           _.extend(stock, StockModel);
           this.stocks.push(stock);
         }
         this.recalculate();
-        savemodel();
+        saveModel();
       },
       removeStock: function (stock) {
-        _.remove(this.stocks, function(s){
+        _.remove(this.stocks, function (s) {
           return s.company.symbol === stock.company.symbol;
         });
         this.recalculate();
         saveModel();
       },
       recalculate: function () {
-        var calcs = _.reduce(this.stocks, function (calcs, stock){
+        var calcs = _.reduce(this.stocks, function (calcs, stock) {
           calcs.shares += stock.shares;
           calcs.marketValue += stock.marketValue;
           calcs.dayChange += stock.dayChange;
           return calcs;
-        }, {shares: 0, marketValue: 0, dayChange: 0});
+        }, { shares: 0, marketValue: 0, dayChange: 0 });
+
         this.shares = calcs.shares;
         this.marketValue = calcs.marketValue;
         this.dayChange = calcs.dayChange;
